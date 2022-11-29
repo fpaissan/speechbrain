7a813056622f78735e2b0adca0d7c95e1bf9474d
fpaissan <me@francescopaissan.it>
2022-11-29 18:59:27 +0000

    implemented static quant + refactoring

--- speechbrain/compression/quantizers.py
+++ speechbrain/compression/quantizers.py
@@ -11,6 +11,44 @@ from typing import List
 import torch.nn as nn
 import speechbrain as sb
 
+def _prepare_model(mod, quant_type):
+    model_to_quantize = copy.deepcopy(mod)
+    model_to_quantize.eval()
+    
+    if quant_type == "dynamic":
+        qconfig_mapping = QConfigMapping().set_global(torch.quantization.default_dynamic_qconfig)
+    elif quant_type == "static":
+        qconfig_mapping = QConfigMapping().set_global(torch.quantization.default_qconfig)
+    
+    return prepare_fx(
+        model_to_quantize,
+        qconfig_mapping,
+        None
+    )
+
+def _static_quantize(modules, data):
+    assert data != None, "Can't quantize without calibration data for Post Training static quantization."
+    if not isinstance(modules, list):
+        modules = [modules]
+    
+    quantized_modules = []
+
+    for m in modules:
+        # prepare
+        model_prepared = _prepare_model(m, "static")
+        
+        # calibrate activations
+        model_prepared.eval()
+        with torch.no_grad():
+            model_prepared(data)
+        
+        # quantize
+        model_quantized = convert_fx(model_prepared)
+
+        quantized_modules.append(model_quantized)
+
+    return quantized_modules
+
 def _dynamic_quantize(modules):
     if not isinstance(modules, list):
         modules = [modules]
@@ -18,21 +56,9 @@ def _dynamic_quantize(modules):
     quantized_modules = []
 
     for m in modules:
-        model_to_quantize = copy.deepcopy(m)
-        model_to_quantize.eval()
-
-        # prepare QMapping for dynamic quantization
-        qconfig_mapping = QConfigMapping().set_global(torch.quantization.default_dynamic_qconfig)
-
         # prepare
-        model_prepared = prepare_fx(
-            model_to_quantize,
-            qconfig_mapping,
-            None
-        )
-
-        # no calibration needed when we only have dynamic/weight_only quantization
-
+        model_prepared = _prepare_model(m, "dynamic")
+        
         # quantize
         model_quantized = convert_fx(model_prepared)
 
@@ -41,13 +67,14 @@ def _dynamic_quantize(modules):
     return quantized_modules
 
 
-def quantize(modules: List[nn.Module], quant_type="dynamic") -> List[nn.Module]:
+def quantize(modules: List[nn.Module], data=None, quant_type="dynamic") -> List[nn.Module]:
     """Quantizes the modules of the brain.
     """
-#    if self.quantization_type == "static":
-#        return self._static_quantize(brain)
+    
     if quant_type == "dynamic":
         return _dynamic_quantize(modules)
+    elif quant_type == "static":
+        return _static_quantize(modules, data)
     else:
         raise NotImplemented(f"Unknown quantization type {quant_type}.")
 
